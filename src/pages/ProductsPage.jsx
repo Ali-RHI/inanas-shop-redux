2d0c1203258137e68dd06abb4c4b0faa28134547
Ali Rouhi <alirhtg@gmail.com>
2024-05-09 12:23:25 +0330

    adding basic css to products page

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,12 +1,14 @@
 import { useProducts } from '../context/ProductsContext.jsx';
 
+import styles from './ProductsPage.module.css';
+
 function ProductsPage() {
 	const products = useProducts();
 	console.log(products);
 
 	return (
 		<div className={styles.container}>
-			<div>
+			<div className={styles.products}>
 				{products.map((p) => (
 					<p key={p.id}>{p.title}</p>
 				))}
