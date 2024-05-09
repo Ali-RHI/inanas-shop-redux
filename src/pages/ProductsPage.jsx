63901802440eb80457bf9e6dc78e603f9d670e14
Ali Rouhi <alirhtg@gmail.com>
2024-05-09 16:51:15 +0330

    adding search bar

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { ImSearch } from 'react-icons/im';
 import Card from '../components/Card.jsx';
 import Loader from '../components/Loader.jsx';
 import { useProducts } from '../context/ProductsContext.jsx';
@@ -7,19 +9,38 @@ import styles from './ProductsPage.module.css';
 function ProductsPage() {
 	const products = useProducts();
 
+	const [search, setSearch] = useState('');
+
+	const searchHandler = () => {};
+
 	return (
-		<div className={styles.container}>
-			<div className={styles.products}>
-				{!products.length && <Loader />}
-				{products.map((p) => (
-					<Card
-						key={p.id}
-						data={p}
-					/>
-				))}
+		<>
+			<div>
+				<input
+					type="text"
+					placeholder="Seach ..."
+					value={search}
+					onChange={(e) =>
+						setSearch(e.target.value.toLowerCase().trim())
+					}
+				/>
+				<button onClick={searchHandler}>
+					<ImSearch />
+				</button>
 			</div>
-			<div>Sidebar</div>
-		</div>
+			<div className={styles.container}>
+				<div className={styles.products}>
+					{!products.length && <Loader />}
+					{products.map((p) => (
+						<Card
+							key={p.id}
+							data={p}
+						/>
+					))}
+				</div>
+				<div>Sidebar</div>
+			</div>
+		</>
 	);
 }
 
