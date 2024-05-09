53ee2269764b363a91e2579b76ac10cd96ed68b0
Ali Rouhi <alirhtg@gmail.com>
2024-05-09 16:37:26 +0330

    adding css to products page

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,16 +1,20 @@
+import Card from '../components/Card.jsx';
 import { useProducts } from '../context/ProductsContext.jsx';
 
 import styles from './ProductsPage.module.css';
 
 function ProductsPage() {
 	const products = useProducts();
-	console.log(products);
 
 	return (
 		<div className={styles.container}>
 			<div className={styles.products}>
+				{!products.length && <p>Loading ...</p>}
 				{products.map((p) => (
-					<p key={p.id}>{p.title}</p>
+					<Card
+						key={p.id}
+						data={p}
+					/>
 				))}
 			</div>
 			<div>Sidebar</div>
