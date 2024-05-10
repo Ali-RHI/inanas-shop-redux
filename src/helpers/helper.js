a16ed2f49e6c6cab925a39aceeeb5a54502d8a9f
Ali Rouhi <alirhtg@gmail.com>
2024-05-10 16:20:35 +0330

    adding search and categories functionality

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -2,4 +2,6 @@ const shortenText = text => {
     return text.split(" ").slice(0,3).join(' ')
 }
 
+
+
 export {shortenText}
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,17 +1,37 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ImSearch } from 'react-icons/im';
 import Card from '../components/Card.jsx';
 import Loader from '../components/Loader.jsx';
 import { useProducts } from '../context/ProductsContext.jsx';
 
 import styles from './ProductsPage.module.css';
+import { FaListUl } from 'react-icons/fa';
 
 function ProductsPage() {
 	const products = useProducts();
 
+	const [displayed, setDisplayed] = useState([]);
 	const [search, setSearch] = useState('');
+	const [query, setQuery] = useState({});
 
-	const searchHandler = () => {};
+	useEffect(() => {
+		setDisplayed(products);
+	}, [products]);
+	useEffect(() => {
+		console.log(query);
+	}, [query]);
+
+	const searchHandler = () => {
+		setQuery({ ...query, search });
+	};
+
+	const categoryHandler = (event) => {
+		const { tagName } = event.target;
+		const category = event.target.innerText.toLowerCase();
+		console.log(category);
+		if (tagName != 'LI') return;
+		setQuery((query) => ({ ...query, category }));
+	};
 
 	return (
 		<>
@@ -30,15 +50,27 @@ function ProductsPage() {
 			</div>
 			<div className={styles.container}>
 				<div className={styles.products}>
-					{!products.length && <Loader />}
-					{products.map((p) => (
+					{!displayed.length && <Loader />}
+					{displayed.map((p) => (
 						<Card
 							key={p.id}
 							data={p}
 						/>
 					))}
 				</div>
-				<div>Sidebar</div>
+				<div>
+					<div>
+						<FaListUl />
+						<p>Categories</p>
+					</div>
+					<ul onClick={categoryHandler}>
+						<li>All</li>
+						<li>Electronics</li>
+						<li>Jewelery</li>
+						<li>Men's Clothing</li>
+						<li>Women's Clothing</li>
+					</ul>
+				</div>
 			</div>
 		</>
 	);
