d7f5ebc17751949d8670eeb0a21821a9ed54ce54
Ali Rouhi <alirhtg@gmail.com>
2024-05-11 13:48:07 +0330

    create sidebar as a component

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -4,7 +4,6 @@ import Loader from '../components/Loader.jsx';
 import { useProducts } from '../context/ProductsContext.jsx';
 
 import styles from './ProductsPage.module.css';
-import { FaListUl } from 'react-icons/fa';
 import {
 	createQueryObject,
 	filterProducts,
@@ -13,6 +12,7 @@ import {
 } from '../helpers/helper.js';
 import { useSearchParams } from 'react-router-dom';
 import SearchBox from '../components/SearchBox.jsx';
+import Sidebar from '../components/Sidebar.jsx';
 
 function ProductsPage() {
 	const products = useProducts();
@@ -29,25 +29,19 @@ function ProductsPage() {
 	}, [products]);
 	useEffect(() => {
 		setSearchParams(query);
-		setSearch(query.search || "");
+		setSearch(query.search || '');
 		let finalProducts = searchProducts(products, query.search);
 		finalProducts = filterProducts(finalProducts, query.category);
 		setDisplayed(finalProducts);
 	}, [query]);
 
-
-	const categoryHandler = (event) => {
-		const { tagName } = event.target;
-		const category = event.target.innerText.toLowerCase();
-		console.log(category);
-		if (tagName != 'LI') return;
-		setQuery((query) => createQueryObject(query, { category }));
-	};
-
 	return (
 		<>
-		<SearchBox search={search} setSearch={setSearch} setQuery={setQuery} />
-			
+			<SearchBox
+				search={search}
+				setSearch={setSearch}
+				setQuery={setQuery}
+			/>
 			<div className={styles.container}>
 				<div className={styles.products}>
 					{!displayed.length && <Loader />}
@@ -58,19 +52,7 @@ function ProductsPage() {
 						/>
 					))}
 				</div>
-				<div>
-					<div>
-						<FaListUl />
-						<p>Categories</p>
-					</div>
-					<ul onClick={categoryHandler}>
-						<li>All</li>
-						<li>Electronics</li>
-						<li>Jewelery</li>
-						<li>Men's Clothing</li>
-						<li>Women's Clothing</li>
-					</ul>
-				</div>
+				<Sidebar setQuery={setQuery} />
 			</div>
 		</>
 	);
